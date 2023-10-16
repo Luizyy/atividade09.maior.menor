be8e2bbd26c2e09cb05a4bb33d42259863f0521d
Luizyy <analuizynascimentodasilva@gmail.com>
2023-10-16 15:15:40 -0300

    numero maior ou menor

---
+++ atividade09.py
@@ -0,0 +1,9 @@
+#Exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+n1 = int(input("digite o primeiro numero inteiro"))
+n2 = int(input("digite o segundo numero inteiro"))
+if n1 >= n2:
+    print (f"O primeiro numero:{n1} é maior ou igual a {n2}")
+else :
+    print(f"O primeiro numero:{n1} é menor que {n2} ")
+
+
